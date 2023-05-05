0791bbe494daf30c23c4fd38ef0adc40ebc48db3
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-05 19:25:15 +0100

    complete

--- src/Child.js
+++ src/Child.js
@@ -3,13 +3,15 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div
+      <div 
+        onClick={() => this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color }}
       ></div>
-    )
-  }
+    );
+  };
 }
 
 export default Child
